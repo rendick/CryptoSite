77f2fe4ff51f8cc7b2333bed56f65c5d77ce7fd6
Naddur <kostanaddur@gmail.com>
2023-01-07 23:57:59 +0200

    about us + loginform

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.scss';
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
+import About from './components/About/About'
 
 function App() {
   return (
     <div className='wrapper'>
       <Header />
       <Main />
+      <About />
     </div>
   );
 }
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
+import './About.scss'
 
 export default class About extends Component {
-  render() {
-    return (
-      <div>About</div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <section>
+                    <div className="menu">
+                        <div className='menu-gui'></div>
+                        <h2 className='menu-title'>About Us</h2>
+                        <p className='main-text'>A Lorem Ipsum is simply dummy text of the printing <br></br>and typesetting industry.
+                            Lorem Ipsum has been the <br /> industry's  standard dummy text ever since the 1500s, <br /> when an unknown <br /> <br /> printing and typesetting industry.
+                            Lorem Ipsum has <br /> been the industry's  standard dummy text ever since <br /> the 1500s, when an unknown</p>
+
+                    </div>
+                </section>
+            </div>
+        )
+    }
 }
