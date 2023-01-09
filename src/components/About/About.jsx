76f17451f8cb006fea1af641d5b24ec5f1324498
Naddur <kostanaddur@gmail.com>
2023-01-09 21:12:34 +0200

    dropdown menu + adoutus

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,8 +10,8 @@ export default class About extends Component {
                     <div className="menu">
                         <div className='menu-gui'></div>
                         <h2 className='menu-title'>About Us</h2>
-                        <p className='main-text'>A Lorem Ipsum is simply dummy text of the printing <br></br>and typesetting industry.
-                            Lorem Ipsum has been the <br /> industry's  standard dummy text ever since the 1500s, <br /> when an unknown <br /> <br /> printing and typesetting industry.
+                        <p className='main-text'>We are one of the crypto-companies that truly cares about <br></br> its clients and also gives
+                            away Airdrop as a bonus. <br /> industry's  standard dummy text ever since the 1500s, <br /> when an unknown <br /> <br /> printing and typesetting industry.
                             Lorem Ipsum has <br /> been the industry's  standard dummy text ever since <br /> the 1500s, when an unknown</p>
                         <a href=""><button className='main-btn'>Learn More</button></a>
                         <div className="group">
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,33 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './Header.scss'
 
-export default class Header extends Component {
-    render() {
-        return (
-            <div className='container'>
-                <header>
-                    <div className="header">
-                        <div className="btn">
-                            <ul>
-                                <li><a href="">About us</a></li>
-                                <li><a href="">Services</a></li>
-                                <li><a href="">How it works</a></li>
-                                <li><a href="">Community</a></li>
-                                <li><a href="https://google.com" target={'_blank'}><button className='airdrop-btn'>Airdrop</button></a></li>
-                            </ul>
-                        </div>
-                        <div className="logo">
-                            <h2>CyberGeeks</h2>
-                        </div>
+function DropdownMenu() {
+    return (
+        <div className='container'>
+            <header>
+                <div className="header">
+                    <div className="btn">
+                        <ul>
+                            <li><a href="">About us</a></li>
+                            <li><a href="">Services</a></li>
+                            <li><a href="">How it works</a></li>
+                            <div class="dropdown">
+                                <li><a href="" className="dropbtn">Community</a></li>
+                                <div class="dropdown-content">
+                                    <a href="#">Telegram</a>
+                                    <a href="#">Twitter</a>
+                                </div>
+                            </div>
+                            <li><a href="https://google.com" target={'_blank'}><button className='airdrop-btn'>Airdrop</button></a></li>
+                        </ul>
                     </div>
-                </header>
-            </div>
-        )
-    }
+                    <div className="logo">
+                        <h2>CyberGeeks</h2>
+                    </div>
+                </div>
+            </header>
+        </div>
+    )
 }
+
+export default DropdownMenu;
