47ad1bcac8cf4ab2964ee40b4ca3b75f6e1cdcc8
Naddur <kostanaddur@gmail.com>
2023-01-09 19:34:04 +0200

    aboutus section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './About.scss'
+import Group from './group.png'
 
 export default class About extends Component {
     render() {
@@ -12,7 +13,10 @@ export default class About extends Component {
                         <p className='main-text'>A Lorem Ipsum is simply dummy text of the printing <br></br>and typesetting industry.
                             Lorem Ipsum has been the <br /> industry's  standard dummy text ever since the 1500s, <br /> when an unknown <br /> <br /> printing and typesetting industry.
                             Lorem Ipsum has <br /> been the industry's  standard dummy text ever since <br /> the 1500s, when an unknown</p>
-
+                        <a href=""><button className='main-btn'>Learn More</button></a>
+                        <div className="group">
+                            <img src={Group} alt="" />
+                        </div>
                     </div>
                 </section>
             </div>
